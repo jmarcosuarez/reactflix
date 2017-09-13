c6b7d6dedf45d4a8da21b5c632ffa86b49c720d4
Marco Suarez <jmarcosuarez@gmail.com>
2017-09-13 23:00:26 +0100

    Add postcss-cssnext -Closes #18

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
+    'postcss-cssnext': {
+      browsers: ['last 2 versions', '> 5%'], // This is for Autoprefixer, which comes built-in
+    },
   },
 };
