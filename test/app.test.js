26ae018237a525ef2c630c8af2f175a24501d2ef
Marco Suarez <jmarcosuarez@gmail.com>
2017-09-13 22:52:43 +0100

    Edit Jest configuration - Closes #20

--- test/app.test.js
+++
@@ -1,7 +0,0 @@
-// This tests ensures Jest is able to run tests
-/* eslint-disable no-undef */
-describe('App', () => {
-  it('should be able to run tests', () => {
-    expect(1 + 2).toEqual(3);
-  });
-});
