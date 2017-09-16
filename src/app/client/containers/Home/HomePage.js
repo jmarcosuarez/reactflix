c6d991383513fe44e173cf1cd3a106a68dbce4c9
Marco Suarez <jmarcosuarez@gmail.com>
2017-09-16 15:33:18 +0100

    Refactor css - separete column markup from initial row markup

--- src/app/client/containers/Home/HomePage.js
+++ src/app/client/containers/Home/HomePage.js
@@ -4,15 +4,17 @@ import styles from './Home.css';
 
 const HomePage = () =>
   <Layout title="Welcome!">
-    <div className={styles.main}>
-      <div className={styles.logo}>
-        <p>Logo</p>
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.logo}>
+          <p>Logo</p>
+        </div>
+
+        <h1>This is the main home Page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <Button to="/">Get started</Button>
+
       </div>
-
-      <h1>This is the main home Page</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <Button to="/">Get started</Button>
-
     </div>
 
   </Layout>;
