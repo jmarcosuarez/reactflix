cad047bce606ad76bd7b061a8d60662f79824461
Marco Suarez <jmarcosuarez@gmail.com>
2017-09-21 21:21:26 +0100

    Refactor styles

--- src/app/client/components/Footer/Footer.js
+++ src/app/client/components/Footer/Footer.js
@@ -5,14 +5,10 @@ const Footer = () => (
   <footer className={styles.footer}>
     <div>
       <p>
-          &copy; ThisCompany Limited 2017 All rights reserved.
-          Except as permitted by the copyright law applicable to you,
-          you may reproduce or communicate any of the content on this website,
-          including files downloadable from this website, without the permission of the copyright owner.
-        </p>
-      <p>
-        We are not going to change these terms of use anytime.
-        No need to check before re-using any content from this website.
+        &copy; ThisCompany Limited 2017 All rights reserved.
+        Except as permitted by the copyright law applicable to you,
+        you may reproduce or communicate any of the content on this website,
+        including files downloadable from this website, without the permission of the copyright owner.
       </p>
     </div>
   </footer>
